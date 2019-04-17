7b22c0eeddca3abf93381a6daa84eb339f51cbbf
Kyle Opperman <kyleopperman1@gmail.com>
2019-04-17 13:07:43 +0200

    ADd description

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -5,7 +5,15 @@ import Verbs from 'components/verbs';
 
 const Home = ({ history }) => (
   <div>
-    <Helmet title='conjugate.pl' />
+    <Helmet>
+      <title>conjugate.pl</title>
+      <meta
+        name='description'
+        content='Welcome to conjugate.pl, this is a very simple app with its main focus
+      on searching for similar words and an easy to use interface that is
+      responsive to mobile'
+      />
+    </Helmet>
     <Verbs history={history} />
   </div>
 );
