93773fa0941d909fe02ac3bc78bab061551bfd41
Kyle Opperman <kyleopperman1@gmail.com>
2020-01-06 12:27:09 +0200

    Revert "Fix data"
    This reverts commit 68a35bce14b21443d32f5ab0d613d9dd3baef2db.

--- src/components/verb/index.js
+++ src/components/verb/index.js
@@ -14,10 +14,8 @@ import {
 // import { KeyboardArrowRight } from 'styled-icons/material';
 
 const Verb = ({ verb, history }) => {
-  // fucked up my data, yolo
-  verb.data[0].pronoun = 'ja';
-  verb.data.splice(1, 0, verb.data.pop());
-  verb.data[1].pronoun = 'ty';
+  const ja = verb.data.pop();
+  verb.data.unshift(ja);
 
   return (
     <VerbCard onClick={() => history.push(`/verb?name=${verb.verb}`)}>
